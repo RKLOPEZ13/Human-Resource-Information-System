9dbf8cd221e728fca86874e1348c29722175747c
NEWNAME <NEWEMAIL>
2026-01-26 10:33:06 +0800

    logo path correction

--- includes/header.php
+++ includes/header.php
@@ -49,7 +49,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
       <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="./main.php" class="logo d-flex align-items-center">
           <img src="assets/img/logo.png" alt="">
           <span class="d-none d-lg-block">HRIS</span>
         </a>
